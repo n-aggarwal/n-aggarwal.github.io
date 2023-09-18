9ccd8e3a88fd74231bd92aa044987699438432f9
Nishant Aggarwal <naggarwal@wesleyan.edu>
2023-09-17 22:55:25 -0400

    added particles

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,13 @@ const Home = () => {
     <div className="bg-primary/60 h-screen">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className=" text-center flex flex-col mt-4 xl:pt-10 xl:text-left h-full container mx-auto">
-          <motion.h1 variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden' className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
           </motion.h1>
@@ -27,16 +33,21 @@ const Home = () => {
       </div>
       {/*Background Image*/}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
-        </div>
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/*Particles*/}
-        <div>Particles</div>
+        <ParticlesContainer />
         {/*Avatar Image */}
-        <motion.div variants={fadeIn('up', 0.2)} initial='hidden' animate='show' exit='hidden' transition={{duration: 1, ease: 'easeInOut'}} className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+        <motion.div
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
           <Avatar />
         </motion.div>
       </div>
-
     </div>
   );
 };
