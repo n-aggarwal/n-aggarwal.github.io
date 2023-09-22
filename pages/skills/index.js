cf8c5d1ff53c4cb2264f70bedc957d8d1cc21406
Nishant Aggarwal <naggarwal@wesleyan.edu>
2023-09-22 14:47:43 -0400

    minor changes

--- pages/skills/index.js
+++ pages/skills/index.js
@@ -82,6 +82,7 @@ const Details = ({ img, expertise, name }) => {
           src={img}
           className="rounded-full border border-gray-500 object-cover w-24 h-24 filter group-hover:grayscale transition duration-300 ease-in-out"
           width={24}
+          alt="Skill Logo"
         ></img>
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 rounded-full z-0">
           <div className="flex items-center justify-center h-full">
